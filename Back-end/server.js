ef3a7e5243256aa0a50580befc1a0d7505889c17
Taomosquito <S_Peloquin123@live.com>
2024-11-13 20:32:03 -0500

    add server file and behaviour

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,15 +1,12 @@
 import express from "express"; // This works with esModuleInterop
-const PORT: number = 3000;
-
+const PORT = 3000;
 const app = express();
-
 // Define a route with types for the request and response objects
 app.get("/", (req, res) => {
-  // No need to type `req` and `res` explicitly if you're fine with implicit types
-  res.send("Hello World");
+    // No need to type `req` and `res` explicitly if you're fine with implicit types
+    res.send("Hello World");
 });
-
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
