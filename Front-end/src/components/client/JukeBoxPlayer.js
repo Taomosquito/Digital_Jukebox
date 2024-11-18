089c76acc2d06e464b367545db75093e5f37e4c7
rusgyn <rusgynmagsanay@gmail.com>
2024-11-18 16:07:03 -0500

    Define Playlist, and JukeBoxPlayer component, and each CSS styling.

---
+++ Front-end/src/components/client/JukeBoxPlayer.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+const JukeBoxPlayer = () => {
+    return (_jsxs("div", { className: "juke-box-player", children: [_jsx("div", { className: "juke-box-player__now-playing", children: "Now Playing" }), _jsx("div", { className: "juke-box-player__next-song", children: "Next in the Playlist" }), _jsx("div", { className: "juke-box-player__list" })] }));
+};
+export default JukeBoxPlayer;
---
+++ Front-end/src/components/client/JukeBoxPlayer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Playlist from '../Playlist';
+
+
+const JukeBoxPlayer = () => {
+  
+  return (
+    <div className="juke-box-player">
+
+      <div className="juke-box-player__now-playing">
+        Now Playing
+      </div>
+
+      <div className="juke-box-player__next-song">
+        Next in the Playlist
+      </div>
+
+      <div className="juke-box-player__list">
+        {/* <Playlist /> */}
+      </div>
+
+
+    </div>
+  );
+};
+
+export default JukeBoxPlayer;
