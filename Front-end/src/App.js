273ea1a4971c6508ad14a358c4ef1cb28d0ec026
rusgyn <rusgynmagsanay@gmail.com>
2024-11-20 15:05:44 -0500

    Modify to apply multiple routes.

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,10 +1,9 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import SideNavigationBar from './components/SideNavigationBar';
 function App() {
     // const [count, setCount] = useState(0)
-    return (_jsx("div", { className: 'App', children: _jsx(SideNavigationBar, {}) }));
+    return (_jsx(Router, { children: _jsx("div", { className: 'App', children: _jsx(SideNavigationBar, {}) }) }));
 }
 export default App;
