fd5cbb32278c7dd351424930baba3177634eba33
rusgyn <rusgynmagsanay@gmail.com>
2024-11-22 15:58:48 -0500

    Implement home page functionality and close all other open components.

--- Front-end/src/components/SideNavigationBar.js
+++ Front-end/src/components/SideNavigationBar.js
@@ -2,25 +2,29 @@ import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-run
 import '../styles/SideNavigation.scss';
 import SearchModal from './SearchSong';
 import { useApplication } from '../hooks/useApplicationData';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const SideNavigation = () => {
-    const { isMenuActive, isModalOpen, isPlaylistOpen, //
-    handleToggleMenu, handleSearchClick, handlePlaylistClick, //
-    handleCloseModal, handleCloseSideNav } = useApplication(); // Use the custom hook for state and functions
+    const { isMenuActive, isModalOpen, isPlaylistOpen, handleToggleMenu, handleSearchClick, handlePlaylistClick, handleHomeClick, handleCloseModal, handleCloseSideNav } = useApplication();
     const navigate = useNavigate();
-    const handlePlaylistNavigation = () => {
+    //Handle home page, close other modals and sideNavigationBar
+    const handleHomeNavigation = () => {
         handleCloseModal(); // Close any open modal
+        handleHomeClick(); //Open the Home Page
+        handleCloseSideNav(); //Close the side navigation
+        navigate('/'); // Navigate to Home route
+    };
+    const handlePlaylistNavigation = () => {
+        handleCloseModal();
         handlePlaylistClick(); // Open the Playlist Modal
-        handleCloseSideNav(); // Close the side navigation
+        handleCloseSideNav();
         navigate('/playlist'); // Navigate to playlist route
     };
-    // Function to handle search modal click and close any other modals
     const handleSearchNavigation = () => {
         handleCloseModal();
         handleSearchClick(); // Open the Search Modal
-        handleCloseSideNav(); // Close the side navigation
-        navigate('/search'); // Navigate to the search route
+        handleCloseSideNav();
+        navigate('/search');
     };
-    return (_jsxs(_Fragment, { children: [_jsx("div", { className: "side-nav-bar__logo", onClick: handleToggleMenu, children: _jsx("i", { className: "fas fa-sliders" }) }), _jsx("div", { className: "side-nav-bar__search-link", onClick: handleSearchNavigation, children: _jsx("i", { className: "fas fa-magnifying-glass" }) }), _jsx("div", { className: `side-nav-bar ${isMenuActive ? 'active' : 'hidden'}`, children: _jsxs("div", { className: "side-nav-bar__icon", children: [_jsxs("div", { className: "side-nav-bar__admin-control", children: [_jsx(Link, { to: "/", children: _jsx("i", { className: "fas fa-house" }) }), _jsx("i", { className: "fas fa-user-plus" }), _jsx("i", { className: "fas fa-magnifying-glass", onClick: handleSearchNavigation }), _jsx("i", { className: "fas fa-heart-circle-xmark" }), _jsx("i", { className: "fas fa-music", onClick: handlePlaylistNavigation }), " "] }), _jsx("br", {}), _jsxs("div", { className: "side-nav-bar__media-control", children: [_jsx("i", { className: "fas fa-circle-pause" }), _jsx("i", { className: "fas fa-forward-step" })] }), _jsx("span", { className: "side-nav-bar__logout", children: _jsx("i", { className: "fas fa-arrow-right-from-bracket" }) })] }) }), _jsx(SearchModal, { isOpen: isModalOpen, onClose: handleCloseModal })] }));
+    return (_jsxs(_Fragment, { children: [_jsx("div", { className: "side-nav-bar__logo", onClick: handleToggleMenu, children: _jsx("i", { className: "fas fa-sliders" }) }), _jsx("div", { className: "side-nav-bar__search-link", children: _jsx("i", { className: "fas fa-magnifying-glass", onClick: handleSearchNavigation }) }), _jsx("div", { className: `side-nav-bar ${isMenuActive ? 'active' : 'hidden'}`, children: _jsxs("div", { className: "side-nav-bar__icon", children: [_jsxs("div", { className: "side-nav-bar__admin-control", children: [_jsx("i", { className: "fas fa-house", onClick: handleHomeNavigation }), _jsx("i", { className: "fas fa-user-plus" }), _jsx("i", { className: "fas fa-magnifying-glass", onClick: handleSearchNavigation }), _jsx("i", { className: "fas fa-heart-circle-xmark" }), _jsx("i", { className: "fas fa-music", onClick: handlePlaylistNavigation })] }), _jsx("br", {}), _jsxs("div", { className: "side-nav-bar__media-control", children: [_jsx("i", { className: "fas fa-circle-pause" }), _jsx("i", { className: "fas fa-forward-step" })] }), _jsx("span", { className: "side-nav-bar__logout", children: _jsx("i", { className: "fas fa-arrow-right-from-bracket" }) })] }) }), _jsx(SearchModal, { isOpen: isModalOpen, onClose: handleCloseModal })] }));
 };
 export default SideNavigation;
