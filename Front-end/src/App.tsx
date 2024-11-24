b2038b8a74d0ae6903701eacb272ab694d634bba
Taomosquito <S_Peloquin123@live.com>
2024-11-23 20:16:10 -0500

    admin able to be added to database

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,30 +1,37 @@
 import React, { useState } from "react";
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import TrackListManager from "./components/TrackListManager";
-import SideNavigationBar from './components/SideNavigationBar'
-import SearchSong from './components/SearchSong'
+import SideNavigationBar from "./components/SideNavigationBar";
+import SearchSong from "./components/SearchSong";
 import PlayList from "./components/Playlist";
-
+import Login from "./components/Login";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <SideNavigationBar />
         <Routes>
-          <Route path="/" element={<SideNavigationBar />} />
-          <Route path="/playlist" element={<PlayList isOpen={false} onClose={function (): void {
-            throw new Error("Function not implemented.");
-          } } />} />
+          <Route
+            path="/playlist"
+            element={
+              <PlayList
+                isOpen={false}
+                onClose={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
-  )
-
+  );
 }
 
 export default App;
---
+++ Front-end/src/hooks/useLoginData.ts
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
+
+export const useLoginData = () => {
+  const [adminLoginUsername, setAdminLoginUsername] = useState<string>("");
+  const [adminLoginPassword, setAdminLoginPassword] = useState<string>("");
+
+  // Handle input change for admin username
+  const adminHandleUser = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("event", event);
+    setAdminLoginUsername(event.target.value);
+  };
+  // Handle input change for admin password
+  const adminHandlePass = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAdminLoginPassword(event.target.value);
+  };
+  // Handle form submission for login attempt
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    sendToAdminDatabase();
+  };
+
+  const sendToAdminDatabase = async () => {
+    try {
+      const response = await axios.post<any>(
+        "http://localhost:3000/admins",
+        { username: adminLoginUsername, password: adminLoginPassword },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error: any) {
+      console.error("Error adding admin:", error);
+      throw new Error("Failed to add admin");
+    }
+  };
+
+  return {
+    adminLoginUsername,
+    adminLoginPassword,
+    adminHandleUser,
+    adminHandlePass,
+    handleSubmit,
+  };
+};
