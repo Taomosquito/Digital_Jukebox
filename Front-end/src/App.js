e220e3367535494ee2e9323d896bf6aad35c491d
rusgyn <rusgynmagsanay@gmail.com>
2024-11-26 02:35:25 -0500

    Implement Web socket contect that manage websocket connection. Wrap the entire App.

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -4,10 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SideNavigationBar from "./components/SideNavigationBar";
 import PlayList from "./components/Playlist";
 import AdminAuthentication from "./components/AdminAuthentication";
+import SearchModal from "./components/SearchSong";
+// Import WebSocketProvider
+import { WebSocketProvider } from './context/WebSocketContext';
 function App() {
-    // const [count, setCount] = useState(0)
-    return (_jsx(Router, { children: _jsxs("div", { className: "App", children: [_jsx(SideNavigationBar, {}), _jsxs(Routes, { children: [_jsx(Route, { path: "/playlist", element: _jsx(PlayList, { isOpen: false, onClose: function () {
-                                    throw new Error("Function not implemented.");
-                                } }) }), _jsx(Route, { path: "/admin-auth", element: _jsx(AdminAuthentication, {}) })] })] }) }));
+    return (
+    // Wrap the whole app with WebSocketProvider
+    _jsx(WebSocketProvider, { children: _jsx(Router, { children: _jsxs("div", { className: "App", children: [_jsx(SideNavigationBar, {}), _jsxs(Routes, { children: [_jsx(Route, { path: "/admin-auth", element: _jsx(AdminAuthentication, {}) }), _jsx(Route, { path: "/search", element: _jsx(SearchModal, { isOpen: false, onClose: function () {
+                                        throw new Error("Function not implemented.");
+                                    } }) }), _jsx(Route, { path: "/playlist", element: _jsx(PlayList, { isOpen: false, onClose: function () {
+                                        throw new Error("Function not implemented.");
+                                    } }) })] })] }) }) }));
 }
 export default App;
